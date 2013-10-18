5b78c320712cc66c05fb6f6d996ec1364763c879
Noam <noamr@outlook.com>
2013-10-17 22:06:59 -0400

    speed moves around

--- haptic/haptic.c
+++ haptic/haptic.c
@@ -14,33 +14,16 @@
 #define GET_VALS    2   // Vendor request that returns 2 unsigned integer values
 #define PRINT_VALS  3   // Vendor request that prints 2 unsigned integer values 
 
-// const float PIN_CONVERSION_FACTOR = 65472;
-const float MAX_VOLTAGE = 3;
-const float MIN_WIDTH = 5.5E-4;
-const float MAX_WIDTH = 2.3E-3;
 const float FREQ = 40e3;
-// const uint16_t PEAK_DETECT_DIFF = 21824;
 const uint16_t ZERO_DUTY = 0;
-const uint16_t HALF_DUTY = 32768;
-const float INTERVAL = 0.02; //for servos
-const uint16_t PULSE_WIDTH = 1 << 8;
-const uint16_t ECHO_TIME = 0b1001 << 7;
-const float PULSE_FREQUENCY = 0.07; //how often we send a pulse
+const uint16_t HALF_DUTY = 32768; //6 volts
+const uint16_t QUARTER_DUTY = 16384; //6 volts
+const uint16_t THREE_QUARTER_DUTY = 49152; //6 volts
 
-uint16_t prev_signal_value;
-uint16_t current_signal_value;
-
-uint16_t send_pulse = 0;
-
-uint16_t get_distance = 0;
-
-uint16_t signal_send_time;
-uint16_t peak_detect_time;
-
-uint16_t time_of_flight;
-
-uint16_t pos;
 uint16_t val1, val2;
+uint16_t slow = 0;
+
+
 
 void VendorRequests(void) {
     WORD temp;
@@ -99,57 +82,48 @@ int16_t main(void) {
     init_timer();
     init_oc();
 
-    //setup the signal input pin
-    pin_digitalIn(&D[4]);
+    pin_digitalOut(&D[2]); //D2
+    pin_digitalOut(&D[3]); //D1
+    pin_digitalOut(&D[4]); //ENA
+    // pin_digitalOut(&D[5]); //IN2
+    // pin_digitalOut(&D[6]); //IN1
+    pin_digitalOut(&D[7]); //SLEW
+    pin_digitalOut(&D[8]); //INV
 
-    val1 = 0;
-    val2 = 0;
-    pos = 0; //16 bit int with binary point in front of the MSB
+    pin_write(&D[2],1); //no tri-stating!
+    pin_write(&D[3],0); //no tri-stating!
+    pin_write(&D[4],1); //Enable the system
+    pin_write(&D[7],0); //low slew rat
+    pin_write(&D[8],0); //don't invert the inputs!
 
     led_on(&led2);
-    timer_setPeriod(&timer2, PULSE_FREQUENCY); //how often we send a pulse
-    timer_start(&timer2);
     timer_setPeriod(&timer3, 0.5); //heartbeat
     timer_start(&timer3);
+    timer_setPeriod(&timer3, 2); //heartbeat
+    timer_start(&timer3);
 
-    oc_servo(&oc1,&D[0],&timer4, INTERVAL,MIN_WIDTH, MAX_WIDTH, pos);
-    oc_servo(&oc2,&D[2],&timer5, INTERVAL,MIN_WIDTH, MAX_WIDTH, pos);
-    oc_pwm(&oc3,&D[3],NULL,FREQ,ZERO_DUTY);
+    oc_pwm(&oc3,&D[6],NULL,FREQ,QUARTER_DUTY);
+    // oc_pwm(&oc3,&D[6],NULL,FREQ,QUARTER_DUTY);
+    while(1){
 
-    printf("Good morning\n");
-
-    InitUSB();                              // initialize the USB registers and serial interface engine
-    while (USB_USWSTAT!=CONFIG_STATE) {     // while the peripheral is not configured...
-        ServiceUSB();                       // ...service USB requests
-    }
-    while (1) {
-        ServiceUSB();
-        pin_write(&D[0],val1);
-        pin_write(&D[2],val2); 
-        //adapted from Patrick and Charlie's approach
-        if (!send_pulse && timer_read(&timer2) < PULSE_WIDTH){
-            send_pulse = 1;
-            pin_write(&D[3],HALF_DUTY);     
-            get_distance = 1;
-        } else if (send_pulse && timer_read(&timer2) >= PULSE_WIDTH) {
-            send_pulse = 0;
-            pin_write(&D[3],ZERO_DUTY); 
-        }
-
-        if (timer_read(&timer2) >= ECHO_TIME)
-        {
-            if (pin_read(&D[4]) && get_distance)
-            {
-                printf("%d\n", timer_read(&timer2));
-                get_distance = 0;
-            }
-        }
-       if (timer_flag(&timer3)) {
+         if (timer_flag(&timer3)) {
             //show a heartbeat and a status message
             timer_lower(&timer3);
+            if(slow){
+                pin_write(&D[6],HALF_DUTY);   
+            } else {
+                pin_write(&D[6],QUARTER_DUTY);
+            }
             led_toggle(&led1);
-        }
-
+            slow = !slow;
+        }   
+        // if (!sw_read(&sw2))
+        // {
+        //     slow = !slow;
+        //     // pin_write(&D[3],slow); //Enable the system
+        //     // pin_write(&D[2],slow); //Enable the system
+        //     led_toggle(&led3);
+        // }
     }
 }
 
