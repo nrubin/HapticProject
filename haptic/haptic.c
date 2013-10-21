b8795696b41bdaacac0d937ea75924a56beb34d6
Noam <noamr@outlook.com>
2013-10-21 12:46:34 -0400

    converted to the correct oc modules

--- haptic/haptic.c
+++ haptic/haptic.c
@@ -21,22 +21,14 @@ const uint16_t QUARTER_DUTY = 16384; //6 volts
 const uint16_t THREE_QUARTER_DUTY = 49152; //6 volts
 const uint16_t FULL_DUTY = 65535; //6 volts
 
-// void __attribute__((interrupt)) _CNInterrupt(void); 
-void __attribute__((interrupt)) _OC1Interrupt(void); 
+//declare interrupts
+void __attribute__((interrupt)) _CNInterrupt(void); 
+void __attribute__((interrupt)) _OC3Interrupt(void); 
+void __attribute__((interrupt)) _OC4Interrupt(void); 
 
 uint16_t val1, val2;
-uint16_t slow = 0;
-// CNEN1 =  1<<14;
-
 uint16_t count;
 uint16_t feedback_current;
-// CNEN1bits
-
-// void SetupTach(void){
-// // CNEN2 = 0b0000000000000010;  //looks like d[0] is RP10 AKA CN17
-// // IFS1 = 0; //Bit 4 of IFS1 is CNIF, the interrupt flag we want to raise/lower
-// // _IEC1 = 1;
-// }
 
 void VendorRequests(void) {
     WORD temp;
@@ -105,51 +97,67 @@ void init_motor(void){
     pin_write(&D[8],0); //don't invert the inputs!    
 }
 
-// void __attribute__((interrupt, auto_psv)) _CNInterrupt(void) {
-//     printf("interrupt!\n");
-//     pin_read(&D[0]);
-//     count = 1;
-//     IFS1bits.CNIF = 0;
-// }
+void __attribute__((interrupt, auto_psv)) _CNInterrupt(void) {
+    IFS1bits.CNIF = 0;
+    // printf("interrupt!\n");
+    pin_read(&D[0]);
+    count = 1;
+}
 
-void __attribute__((interrupt, auto_psv)) _OC1Interrupt(void) {
-    IFS0bits.OC1IF = 0;
+void __attribute__((interrupt, auto_psv)) _OC3Interrupt(void) {
+    IFS0bits.OC3IF = 0;
     // printf("count: %d \n", count);
-    count++;
-    val2 = count;
+    // count++;
+    // val2 = count;
     // led_toggle(&led3);
 }
 
+void __attribute__((interrupt, auto_psv)) _OC4Interrupt(void) {
+    IFS1bits.OC4IF = 0;
+    // printf("count: %d \n", count);
+    // count++;
+    // val2 = count;
+    // led_toggle(&led3);
+}
 
-
-int16_t main(void) {
-    // SetupTach();
+void init(void){
     init_pin();
     init_clock();
     init_uart();
     init_ui();
     init_timer();
     init_oc();
-    // init_motor();
+    init_motor();
     InitUSB(); // initialize the USB registers and serial interface engine    
-    count = 0;
-    // CNEN1bits.CN14IE = 1;  //sets the second bit of CNEN1, CN1IE (change notif 1 interrupt enable)
-    // IFS1bits.CNIF = 0; //make sure the interrupt flag is set low
-    // IEC1bits.CNIE = 1; //make sure all change notif inputs are on
+}
+
+void init_interrupts(void){
+    CNEN1bits.CN14IE = 1;  //sets the second bit of CNEN1, CN1IE (change notif 1 interrupt enable)
+    IFS1bits.CNIF = 0; //make sure the interrupt flag is set low
+    IEC1bits.CNIE = 1; //make sure all change notif inputs are on
+
     // CNPD1bits.CN14PDE = 1; //enable the pulldown resistor on CN14
-    IEC0bits.OC1IE = 1;
-    IFS0bits.OC1IF = 0;
+
+    // enable OC3 and OC4 interrupts
+    IEC1bits.OC3IE = 1; 
+    IEC1bits.OC4IE = 1;
+    //Set OC interrupt flags low
+    IFS0bits.OC3IF = 0;
+    IFS1bits.OC4IF = 0;
+}
+
+
+int16_t main(void) {
+    count = 0;
+
     led_off(&led3);
     led_on(&led2);
     timer_setPeriod(&timer3, 0.5);
     timer_start(&timer3);
-    timer_setPeriod(&timer1, 1);
-    timer_start(&timer1);
     // printf("count: %d \n", count);
 
-    // oc_pwm(&oc3,&D[5],NULL,FREQ,FULL_DUTY);
-    oc_pwm(&oc1,&D[5],NULL,FREQ,HALF_DUTY);
-    // oc_pwm(&oc3,&D[6],NULL,FREQ,QUARTER_DUTY);
+    oc_pwm(&oc3,&D[5],NULL,FREQ,QUARTER_DUTY);
+    oc_pwm(&oc4,&D[6],NULL,FREQ,QUARTER_DUTY);
     
     while (USB_USWSTAT!=CONFIG_STATE) {     // while the peripheral is not configured...
         ServiceUSB();                       // ...service USB requests
@@ -164,15 +172,7 @@ int16_t main(void) {
             // feedback_current = pin_read(&A[0]);
             // val2 = feedback_current;
             // printf("count: %d \n", count);
-        }   
-         if (timer_flag(&timer1)) {
-            //show a heartbeat
-            timer_lower(&timer1);
-            count = 0;
-            // feedback_current = pin_read(&A[0]);
-            // val2 = feedback_current;
-            // printf("count: %d \n", count);
-        }  
+        }     
     }
 }
 
